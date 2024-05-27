27101d9f2138eefb3d7ce38341fa8bddcdcf36ea
Raizafarwin3971 <raiza.farwin05@gmail.com>
2024-05-27 13:10:38 +0530

    initial commit

---
+++ src/pages/homepg.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from '../components/header'
+import Footer from '../components/footer'
+import Hero from '../components/hero'
+
+const homepg = () => {
+  return (
+    <div>
+        <Header/>
+        <Hero/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default homepg
